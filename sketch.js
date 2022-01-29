6c51f378d9ec84db6c50fa7ba2d08fce740f016a
aarna-suresh <94156873+aarna-suresh@users.noreply.github.com>
2022-01-28 17:31:45 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,129 @@
+//Infinte Run Game by Aarna using JS
+
+
+//Declare variables for game objects and behaviour indicators(FLAGS)
+var towerImg, tower;
+var doorImg, door, doorsGroup;
+var climberImg, climber, climbersGroup;
+var ghost, ghostImg;
+var invisibleBlockGroup, invisibleBlock;
+var gameState = "play"
+
+function preload(){
+  //Create Media library and load to use it during the course of the software
+  //executed only once at the start of the program
+  towerImg = loadImage("tower.png");
+  doorImg = loadImage("door.png");
+  climberImg = loadImage("climber.png");
+  ghostImg = loadImage("ghost-standing.png");
+  spookySound = loadSound("spooky.wav");
+}
+//define the intial environment of the software(before it is used)
+//by defining the declared variables with default values
+//executed only once at the start of the program
+function setup(){
+  createCanvas(600,600);
+  spookySound.loop();
+  tower = createSprite(300,300);
+  tower.addImage("tower",towerImg);
+  tower.velocityY = 1;
+  
+  doorsGroup = new Group();
+  climbersGroup = new Group();
+  invisibleBlockGroup = new Group();
+  
+  //creation of ghost sprite object
+  ghost = createSprite(200,200,50,50);
+  ghost.scale = 0.3;
+  ghost.addImage("ghost", ghostImg);
+  ghost.debug = false;
+  
+  //creation of inv base
+   invBase = createSprite(300,510,600,5);
+  invBase.visible = false;
+}
+//All modifications, changes, conditions, manipulations, actions during the course of the program are written inside function draw.
+//All commands that are supposed to be executed and checked continously or applied throughout the program are written inside function draw.
+//function draw is executed for every frame created since the start of the program.
+function draw(){
+  background(0);
+  if (gameState === "play") {
+    if(keyDown("left_arrow")){
+      ghost.x = ghost.x - 3;
+    }
+    
+    if(keyDown("right_arrow")){
+      ghost.x = ghost.x + 3;
+    }
+    
+    if(keyDown("space")){
+      ghost.velocityY = -10;
+    }
+    
+    ghost.velocityY = ghost.velocityY + 0.8
+    
+    if(tower.y > 400){
+      tower.y = 300
+    }
+    spawnDoors();
+
+    
+    //climbersGroup.collide(ghost);
+    if(climbersGroup.isTouching(ghost)){
+      ghost.velocityY = 0;
+    }
+    if(invisibleBlockGroup.isTouching(ghost) || ghost.y > 600){
+      ghost.destroy();
+      gameState = "end"
+    }
+    
+    drawSprites();
+  }
+  
+  if (gameState === "end"){
+    stroke("yellow");
+    fill("yellow");
+    textSize(30);
+    text("Game Over", 230,250)
+  }
+  ghost.collide(invBase);
+
+}
+
+function spawnDoors() {
+  //write code here to spawn the doors in the tower
+  if (frameCount % 240 === 0) {
+    var door = createSprite(200, -50);
+    var climber = createSprite(200,10);
+    var invisibleBlock = createSprite(200,15);
+    invisibleBlock.width = climber.width;
+    invisibleBlock.height = 2;
+    
+    door.x = Math.round(random(120,400));
+    climber.x = door.x;
+    invisibleBlock.x = door.x;
+    
+    door.addImage(doorImg);
+    climber.addImage(climberImg);
+    
+    door.velocityY = 1;
+    climber.velocityY = 1;
+    invisibleBlock.velocityY = 1;
+    
+    ghost.depth = door.depth;
+    ghost.depth +=1;
+   
+    //assign lifetime to the variable
+    door.lifetime = 800;
+    climber.lifetime = 800;
+    invisibleBlock.lifetime = 800;
+
+    
+    //add each door to the group
+    doorsGroup.add(door);
+    climbersGroup.add(climber);
+    invisibleBlock.debug = false;
+    invisibleBlock.visible = false;
+    invisibleBlockGroup.add(invisibleBlock);
+  }
+}
